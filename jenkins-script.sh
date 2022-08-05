146aca09d9c05cfb9ad4f08a4e925c5c1e4b5f4f
konrad <konradd1998@gmail.com>
2022-08-05 06:01:32 +0000

    production env updated

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -12,20 +12,14 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report=html
 
-ssh jenkins@production-server << EOF
-if [ -d book-library-app1 ]; then
-  cd book-library-app1 && git pull original main
-else
-  git clone https://github.com/KonradKantorski/flask-project.git book-library-app1
-  cd book-library-app1
-fi
 
-sudo apt install python3 python3-pip python3-venv -y
+while getopts "c" opt; do
+  case ${opt} in
+   c) echo -e "python create.py\n" >> deployment-steps;;
+  esac
+done
 
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
+echo "python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app" >> deployment-steps 
 
-python3 create.py
-python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app
-EOF
+
+ssh jenkins@production-server < deployment-steps 
